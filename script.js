0f16e9445a19e5154f31857c8977b0af1c33fc65
IOS2004 <lostlevy@gmail.com>
2024-08-29 19:29:58 +0530

    Bug fixed

--- script.js
+++ script.js
@@ -14,8 +14,22 @@ function addButtons()
   for (let i = 0; i < 20; i++)
   {
     let item = document.createElement('button');
-    item.id = 'button-card';
+    item.classList = 'button-card';
+    item.id = 'button' + a[i];
     item.textContent = a[i];
+    switch (a[i])
+    {
+      case '/':
+      case '×':
+      case '+':
+      case '-':
+      case '%':
+      case '←':
+      case 'AC': 
+        item.style.backgroundColor = '#aaaaaa50';
+        item.addEventListener('mouseover', () => { item.style.opacity = 0.75 });
+        item.addEventListener('mouseout', () => { item.style.opacity = 1 });
+    }
     buttons.appendChild(item);
     if (a[i] == '=')
     {
@@ -51,6 +65,11 @@ function compute (str)
   // compute a set of operand at a time from right to left
   let ind = 0;
   let comp = '';
+  if (str[ind] == '-')
+  {
+    comp = '-';
+    ind++;
+  }
   while (ind < str.length && ((str[ind] <= 9 && str[ind] >= 0) || str[ind] == '.'))
   {
     comp += str[ind];
@@ -60,6 +79,11 @@ function compute (str)
   let oper = str[i];
   i++;
   let op2 = '';
+  if (str[i] == '-')
+  {
+    op2 = '-';
+    i++;
+  }
   while (i < str.length && ((str[i] <= 9 && str[i] >= 0) || str[i] == '.'))
   {
     op2 += str[i];
@@ -67,14 +91,17 @@ function compute (str)
   }
   comp = evaluate(Number(comp), Number(op2), oper);
   ind = i;
-  if (comp === NaN)
+  if (isNaN(comp))
     return 'Invalid Input';
   return (typeof comp == 'number' ) ? comp.toFixed(10) : comp;
 }
 
 function isValidComp(str) 
 {
-  let a = 0;
+  if (!isNaN(Number(str)))
+    return false;
+  if (isOperEnd(str))
+    return false;
   for (let i = 0; i < str.length; i++)
   {
     switch (str[i]) {
@@ -83,7 +110,6 @@ function isValidComp(str)
       case '+':
       case '-':
       case '/':
-        a++;
         return true;
     }
   }
@@ -93,9 +119,36 @@ function isValidComp(str)
 function convInt(str)
 {
   let j = str.length - 1;
-  while (str[j] == '0' || str[j] == '.')
+  while (j > 0 && (str[j] == '0' || str[j] == '.'))
     j--;
   return str.substr(0, j + 1);
+}
+
+function isOperEnd(str)
+{
+  switch (str[str.length - 1]) {
+    case '%':
+    case '×':
+    case '+':
+    case '-':
+    case '/':
+      return true;
+    default:
+      return false;
+  }
+}
+
+function disableDecimal(str)
+{
+  // check if the current operand is allowed to use decimal
+  let i = str.length - 1;
+  while (i >= 0 && !isOperEnd(str[i]))
+  {
+    if (str[i] == '.')
+      return true;
+    i--;
+  }
+  return false;
 }
 
 buttons.addEventListener('click', (e) => {
@@ -105,6 +158,10 @@ buttons.addEventListener('click', (e) => {
     let str = b.textContent;
     if (output.textContent == '0' && (str >= '0' && str <= '9' || str == '.'))
       output.textContent = '';
+    if (output.textContent[0] == 'I' || output.textContent == 'NaN')
+    {
+      output.textContent = '';
+    }
     switch (str)
     {
       case '%':
@@ -114,9 +171,13 @@ buttons.addEventListener('click', (e) => {
       case '/':
         if (isValidComp(output.textContent))
           output.textContent = convInt(compute(output.textContent));
+        if (isOperEnd(output.textContent))
+          output.textContent = output.textContent.slice(0, output.textContent.length - 1);
         output.textContent = output.textContent + str;
         break;
       case '.':
+        if (disableDecimal(output.textContent))
+          break;
         let s = output.textContent[output.textContent.length - 1];
         if (s >= '1' && s <= '9')
           output.textContent = output.textContent +'.';
@@ -124,7 +185,12 @@ buttons.addEventListener('click', (e) => {
           output.textContent = output.textContent +'0.';
         break;
       case '=':
-        output.textContent = convInt(compute(output.textContent));
+        if (isValidComp(output.textContent))
+        {
+          output.textContent = convInt(compute(output.textContent));
+        }
+        if (output.textContent == '')
+          output.textContent = '0';
         break;
       case 'AC':
         output.textContent = '0';
@@ -135,13 +201,30 @@ buttons.addEventListener('click', (e) => {
           output.textContent = '0';
         break;
       case '√x':
-        if (!isValidComp(output.textContent))
+        if (!isNaN(Number(output.textContent)))
         {
           output.textContent = convInt(Math.sqrt(Number(output.textContent)).toFixed(10));
+          if (isNaN(output.textContent))
+            output.textContent =  'Invalid Input';
         }
         break;
       default: 
         output.textContent = output.textContent + str;
     }
+  }
+})
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter')
+    document.getElementById('button' + '=').click();
+  else if (event.key === 'Backspace')
+    document.getElementById('button' + '←').click();
+  else if (event.key == '*')
+    document.getElementById('button' + '×').click();
+  else 
+  {
+    let b = document.getElementById('button' + event.key);
+    if (b != null)
+      b.click();
   }
 })
