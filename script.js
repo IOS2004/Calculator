47c5576eca929341b8c7814ce93a589122b62e7f
IOS2004 <lostlevy@gmail.com>
2024-08-29 08:40:39 +0530

    Add logic

--- script.js
+++ script.js
@@ -27,9 +27,75 @@ function addButtons()
 }
 addButtons();
 
+function evaluate (op1, op2, oper)
+{
+  switch (oper)
+  {
+    case '+':
+      return op1 + op2;
+    case '-':
+      return op1 - op2;
+    case '×':
+      return op1 * op2;
+    case '/':
+      return op1 / op2;
+    case '%':
+      return op1 % op2;
+    default:
+      return 'Invalid input'
+  }
+}
+
 function compute (str) 
 {
-  return '69';
+  // compute a set of operand at a time from right to left
+  let ind = 0;
+  let comp = '';
+  while (ind < str.length && ((str[ind] <= 9 && str[ind] >= 0) || str[ind] == '.'))
+  {
+    comp += str[ind];
+    ind++;
+  }
+  let i = ind;
+  let oper = str[i];
+  i++;
+  let op2 = '';
+  while (i < str.length && ((str[i] <= 9 && str[i] >= 0) || str[i] == '.'))
+  {
+    op2 += str[i];
+    i++;
+  }
+  comp = evaluate(Number(comp), Number(op2), oper);
+  ind = i;
+  if (comp === NaN)
+    return 'Invalid Input';
+  return (typeof comp == 'number' ) ? comp.toFixed(10) : comp;
+}
+
+function isValidComp(str) 
+{
+  let a = 0;
+  for (let i = 0; i < str.length; i++)
+  {
+    switch (str[i]) {
+      case '%':
+      case '×':
+      case '+':
+      case '-':
+      case '/':
+        a++;
+        return true;
+    }
+  }
+  return false;
+}
+
+function convInt(str)
+{
+  let j = str.length - 1;
+  while (str[j] == '0' || str[j] == '.')
+    j--;
+  return str.substr(0, j + 1);
 }
 
 buttons.addEventListener('click', (e) => {
@@ -37,11 +103,19 @@ buttons.addEventListener('click', (e) => {
   {
     let b = e.target;
     let str = b.textContent;
-    console.log(str);
-    if (output.textContent == '0')
+    if (output.textContent == '0' && (str >= '0' && str <= '9' || str == '.'))
       output.textContent = '';
     switch (str)
     {
+      case '%':
+      case '×':
+      case '+':
+      case '-':
+      case '/':
+        if (isValidComp(output.textContent))
+          output.textContent = convInt(compute(output.textContent));
+        output.textContent = output.textContent + str;
+        break;
       case '.':
         let s = output.textContent[output.textContent.length - 1];
         if (s >= '1' && s <= '9')
@@ -50,7 +124,7 @@ buttons.addEventListener('click', (e) => {
           output.textContent = output.textContent +'0.';
         break;
       case '=':
-        output.textContent = compute(output.textContent);
+        output.textContent = convInt(compute(output.textContent));
         break;
       case 'AC':
         output.textContent = '0';
@@ -59,6 +133,12 @@ buttons.addEventListener('click', (e) => {
         output.textContent = output.textContent.slice(0, output.textContent.length - 1);
         if (output.textContent == '')
           output.textContent = '0';
+        break;
+      case '√x':
+        if (!isValidComp(output.textContent))
+        {
+          output.textContent = convInt(Math.sqrt(Number(output.textContent)).toFixed(10));
+        }
         break;
       default: 
         output.textContent = output.textContent + str;
